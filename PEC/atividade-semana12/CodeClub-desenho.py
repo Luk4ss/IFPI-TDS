b451136290185927290d5c23bd07cca679a1ff00
lucas <lucasregoaraujo@gmail.com>
2022-05-27 11:31:42 -0300

    drawStar, drawPlanet implementados

--- PEC/atividade-semana12/CodeClub-desenho.py
+++ PEC/atividade-semana12/CodeClub-desenho.py
@@ -11,9 +11,32 @@ def drawSquare():
     end_fill()
     penup()
 
+
+def drawStar(size, cor):
+    color(cor)
+    pendown()
+    begin_fill()
+    for side in range(5):
+        left(144)
+        forward(size)
+    end_fill()
+    penup()
+
+
+def drawPlanets(size, cor):
+    color(cor)
+    pendown()
+    begin_fill()
+    for _ in range(360):
+        forward(size)
+        right(1)
+    end_fill()
+    penup()
+
+
 color("Red")
 bgcolor("MidnightBlue")
-
+speed(11)
 
 drawSquare()
 forward(100)
@@ -21,6 +44,18 @@ drawSquare()
 left(90)
 forward(150)
 drawSquare()
-
+backward(200)
+drawStar(50, "orange")
+backward(200)
+drawStar(100, "green")
+forward(150)
+right(90)
+backward(200)
+drawPlanets(1, "red")
+backward(200)
+drawPlanets(2, "orange")
 hideturtle()
 done()
+
+
+
