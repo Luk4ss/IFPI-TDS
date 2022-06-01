70e376d235120995425cb12a4343d44c8bd51aa8
lucas <lucasregoaraujo@gmail.com>
2022-06-01 10:45:10 -0300

    Sem13T1Q5 resolvida

---
+++ PEC/atividade-semana13/Sem-13-T1-Q5.py
@@ -0,0 +1,18 @@
+lista_A = []
+lista_B = []
+
+for _ in range(5):
+    lista_A.append((int(input().strip())))
+
+for _ in range(5):
+    lista_B.append((int(input().strip())))
+
+lista_C = []
+
+for i in range(5):
+    lista_C.append(lista_A[i])
+    lista_C.append(lista_B[i])
+
+print(lista_A)
+print(lista_B)
+print(lista_C)
