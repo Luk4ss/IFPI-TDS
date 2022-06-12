60f6de45a436281d20da10835280a866c3d53b5a
lucas <lucasregoaraujo@gmail.com>
2022-06-12 15:28:19 -0300

    Sem14T1Q3 resolvida

---
+++ PEC/atividade-semana14/cidades.py
@@ -0,0 +1,19 @@
+def carrega_cidades():
+    resultado = []
+    with open('cidades.csv', 'r', encoding='utf-8') as arquivo:
+        for linha in arquivo:
+            uf, ibge, nome, dia, mes, pop = linha.split(';')
+            resultado.append(
+                (uf, int(ibge), nome, int(dia), int(mes), int(pop))
+            )
+    arquivo.close()
+    return resultado
+
+
+def main():
+    cidades = carrega_cidades()
+    print(cidades[:3] + cidades[-2:])
+
+
+if __name__ == '__main__':
+    main()
