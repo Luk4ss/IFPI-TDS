f6fb4978f5031bffe030c3335bfdc5ea66d39f1e
lucas <lucasregoaraujo@gmail.com>
2022-08-21 11:53:59 -0300

    L1Q10 e L1Q8 refatoradas.

--- MODULO-02/DESENVOLVIMENTO-ORIENTADO-A-TESTES/L1-Q10.py
+++ MODULO-02/DESENVOLVIMENTO-ORIENTADO-A-TESTES/L1-Q10.py
@@ -1,25 +1,25 @@
 # LISTA 1 - QUESTÃO 10
 
-def max(a, b, c, d ):
-   maior = a
-   if b >= maior:
-     maior = b
-   if c >= maior:
-     maior = c
-   if d >= maior:
-     maior = d
-   return maior
+def max(a, b, c, d):
+    maior = a
+    if b >= maior:
+        maior = b
+    if c >= maior:
+        maior = c
+    if d >= maior:
+        maior = d
+    return maior
 
 
 for i in range(1, 5):
     while True:
         print(f' >>> Série No#{i} <<<')
         try:
-            a = int(input('Número 1: '))
-            b = int(input('Número 2: '))
-            c = int(input('Número 3: '))
-            d = int(input('Número 4: '))
+            n1 = int(input('Número 1: '))
+            n2 = int(input('Número 2: '))
+            n3 = int(input('Número 3: '))
+            n4 = int(input('Número 4: '))
             break
         except:
             print('### Dado inválido! Por favor, digite novamente! ###\n')
-    print(f'>>>> O maior número dessa série é: {max(a, b, c, d)}\n')
+    print(f'>>>> O maior número dessa série é: {max(n1, n2, n3, n4)}\n')
--- MODULO-02/DESENVOLVIMENTO-ORIENTADO-A-TESTES/L1-Q8.py
+++ MODULO-02/DESENVOLVIMENTO-ORIENTADO-A-TESTES/L1-Q8.py
@@ -1,24 +1,19 @@
 # LISTA 1 - QUESTÃO 8
 
 def ler_caractere(c):
-    if c in ['S', 's']:
-        return c
-    if c in ['N', 'n']:
-        return c
-    else:
-        return ' !!!! Caractere inválido! Digite novamente!!!!'
+    while True:
+        if c in ['S', 's', 'N', 'n']:
+            return c
+        c = input('Caractere inválido! Digite novamente: ')
 
 
 while True:
     try:
         numero = float(input('Digite um Número: '))
         print(f'O cubo desse número é: {numero ** 3: .2f}')
-        while True:
-            opt = input('Deseja continuar? ')
-            res = ler_caractere(opt)
-            print(f'Caractere digitado: {res}')
-            if res in ['N', 'S', 'n', 's']:
-                break
+        opt = input('Deseja continuar? ')
+        res = ler_caractere(opt)
+        print(f'Caractere digitado: {res}')
         if res in ['N', 'n']:
             break
     except:
