9c4f01b7a57bee3aa888efffacfb78a3bfcabbe4
Luk4ss <lucasregoaraujo@gmail.com>
2022-09-21 09:21:52 -0300

    add descricao L2-Q10 de DOT. Questao repetida.

--- MODULO-02/DESENVOLVIMENTO-ORIENTADO-A-TESTES/LISTA 2/L2-Q10.py
+++ MODULO-02/DESENVOLVIMENTO-ORIENTADO-A-TESTES/LISTA 2/L2-Q10.py
@@ -1,3 +1,6 @@
 '''10) Faça um programa que grave uma lista com 15 posições, calcule e mostre:
 a) O maior elemento da lista e em que posição esse elemento se encontra;
 b) O menor elemento da lista e em que posição esse elemento se encontra.'''
+
+
+# Questão repetida!!!! Olhar a questão 4 da lista 2!
