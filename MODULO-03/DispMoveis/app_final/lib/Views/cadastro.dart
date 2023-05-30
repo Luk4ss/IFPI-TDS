9b583eeb529ac8ae3e688e822c69810153936e43
lucas <lucasregoaraujo@gmail.com>
2023-05-30 14:50:36 -0300

    Tela de Login e Cadastro com mensagens

--- MODULO-03/DispMoveis/app_final/lib/Views/cadastro.dart
+++ MODULO-03/DispMoveis/app_final/lib/Views/cadastro.dart
@@ -2,97 +2,156 @@ import 'package:flutter/material.dart';
 
 import '../Controllers/cadastro_controller.dart';
 
-class Cadastro extends StatelessWidget {
+class Cadastro extends StatefulWidget {
   Cadastro({super.key});
 
+  @override
+  State<Cadastro> createState() => _CadastroState();
+}
+
+class _CadastroState extends State<Cadastro> {
+  
   final TextEditingController _loginController = TextEditingController();
   final TextEditingController _senhaController = TextEditingController();
-  final TextEditingController _senhaConfirmaController = TextEditingController();
+  final TextEditingController _senhaConfirmaController =
+      TextEditingController();
 
-  final String mensagem="";
-  final Color? cor = Colors.red;
+  String _msgLogin = "";
+  String _msgSenha = "";
+  String _msgSenhaConfirma = "";
 
-  Future<bool> _fazerCadastro() async{
+  void _limparCampos(){
+    setState(() {
+      _msgLogin = "";
+      _msgSenha = "";
+      _msgSenhaConfirma = "";
+    });
+  }
 
-
-    if(_loginController.text.isEmpty || _senhaController.text.isEmpty || _senhaConfirmaController.text.isEmpty){
+  Future<bool> _fazerCadastro() async {
+    if (_loginController.text.isEmpty) {
+      setState(() {
+        _msgLogin = "Preencha o campo de login!";
+      });
       return false;
     }
 
-    if(_senhaController.text != _senhaConfirmaController.text){
+    setState(() {
+      _msgLogin = "";
+    });
+
+    if (_senhaController.text.isEmpty) {
+      setState(() {
+        _msgSenha = "Preencha sua senha!";
+      });
       return false;
     }
 
-    CadastroController cadastroController = CadastroController(_loginController.text, _senhaController.text );
+    setState(() {
+      _msgSenha = "";
+    });
+
+    if (_senhaController.text != _senhaConfirmaController.text) {
+      setState(() {
+        _msgSenhaConfirma = "As senhas são diferentes!";
+      });
+      return false;
+    }
+
+    setState(() {
+      _msgSenhaConfirma = "";
+    });
+
+    CadastroController cadastroController =
+        CadastroController(_loginController.text, _senhaController.text);
     return await cadastroController.cadastraUsuario();
-
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Cadastro')
-      ),
+      appBar: AppBar(title: const Text('Cadastro')),
       body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[            
-            const SizedBox(height: 30.0,),
-            const Text('Login'),
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: TextField(
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'Entre com o seu login',
-                ),
-                controller: _loginController,
-                ),
+          child: Column(
+        children: <Widget>[
+          const SizedBox(
+            height: 30.0,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: TextField(
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Entre com o seu login',
+                label: Text('Login')
+              ),
+              controller: _loginController,
             ),
-            const SizedBox(height: 15.0,),
-            const Text('Senha'),
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: TextField(
-                controller: _senhaController,
-                obscureText: true,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'Digite a sua senha',
-                ),),
+          ),
+          Text(
+            _msgLogin,
+            style: const TextStyle(color: Colors.red),
+          ),
+          const SizedBox(
+            height: 15.0,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: TextField(
+              controller: _senhaController,
+              obscureText: true,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Digite a sua senha',
+                label: Text('Senha')
+              ),
             ),
-            const SizedBox(height: 15.0,),
-            const Text('Cofirme a sua senha'),
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: TextField(
-                controller: _senhaConfirmaController,
-                obscureText: true,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'Digite a sua senha novamente',
-                ),),
+          ),
+          Text(
+            _msgSenha,
+            style: const TextStyle(color: Colors.red),
+          ),
+          const SizedBox(
+            height: 15.0,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: TextField(
+              controller: _senhaConfirmaController,
+              obscureText: true,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Digite a sua senha novamente',
+                label: Text('Confirmação de Senha')
+              ),
             ),
-            const SizedBox(height: 15.0,),
-            Text(
-              mensagem, 
-              style: TextStyle(color: cor),),
-            const SizedBox(height: 15.0,),
-            ElevatedButton(
-                onPressed: () async {
-                    if(await _fazerCadastro()){
-                        Navigator.pushNamed(context, "login");
-                    }
-                }, 
-                child: const Text('Fazer cadastro')),
-            TextButton(
-              onPressed: (){
+          ),
+          Text(
+            _msgSenhaConfirma,
+            style: const TextStyle(color: Colors.red),
+          ),
+          const SizedBox(
+            height: 15.0,
+          ),
+          ElevatedButton(
+              onPressed: () async {
+                if (await _fazerCadastro()) {
+                  _limparCampos();
+                  Navigator.pushNamed(context, "login");
+                }
+              },
+              child: const Text('Fazer cadastro')),
+          TextButton(
+              onPressed: () {
+                _limparCampos();
                 Navigator.pop(context);
-              }, 
-              child: const Text('voltar', 
-                              style: TextStyle(decoration: TextDecoration.underline),))
-          ],
-        )),
+              },
+              child: const Text(
+                'voltar',
+                style: TextStyle(decoration: TextDecoration.underline),
+              ))
+        ],
+      )),
     );
   }
 }
--- MODULO-03/DispMoveis/app_final/lib/Views/login.dart
+++ MODULO-03/DispMoveis/app_final/lib/Views/login.dart
@@ -1,87 +1,143 @@
-
 import 'package:app_final/Controllers/login_controller.dart';
 import 'package:app_final/Models/pessoa.dart';
 import 'package:flutter/material.dart';
 
-class Login extends StatelessWidget {
-  Login({super.key, title});
+class Login extends StatefulWidget {
+  const Login({super.key, title});
 
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
   final TextEditingController _loginController = TextEditingController();
   final TextEditingController _senhaController = TextEditingController();
 
-  Future<bool> _existeUsuarioCadastrado() async{
-    
-    if(_loginController.text.isEmpty || _senhaController.text.isEmpty){
+  String _msgCampoLogin = "";
+  String _msgCampoSenha = "";
+  String _msgExisteUsuario = "";
+
+  void _limparCampos() {
+    setState(() {   
+      _loginController.text = "";
+      _senhaController.text = "";
+      _msgCampoLogin = "";
+      _msgCampoSenha = "";
+      _msgExisteUsuario = "";
+    });
+  }
+
+  Future<bool> _existeUsuarioCadastrado() async {
+    if (_loginController.text.isEmpty) {
+      setState(() {
+        _msgCampoLogin = "Preencha o campo de Login!";
+      });
       return false;
     }
 
-    var loginController = LoginController(_loginController.text,_senhaController.text); 
+    setState(() {
+      _msgCampoLogin = "";
+    });
+
+    if (_senhaController.text.isEmpty) {
+      setState(() {
+        _msgCampoSenha = "Preencha sua Senha!";
+      });
+      return false;
+    }
+
+    setState(() {
+      _msgCampoSenha = "";
+    });
+
+    var loginController =
+        LoginController(_loginController.text, _senhaController.text);
     Pessoa user = await loginController.retornaUsuarioCadastrado();
 
     //Se o 'id' for vazio, quer dizer que não encontrou usuário no banco.
     return user.id.isNotEmpty;
-
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('LOGIN')
-      ),
+      appBar: AppBar(title: const Text('LOGIN')),
       body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,               
-              children: [
-                SizedBox(
+          child: Column(
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
                   height: 150,
-                  child: Image.asset('images\\ifpi-logo.jpeg', fit: BoxFit.cover,))
-              ],              
+                  child: Image.asset(
+                    'images\\ifpi-logo.jpeg',
+                    fit: BoxFit.cover,
+                  ))
+            ],
+          ),
+          const SizedBox(
+            height: 30.0,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: TextField(
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(), 
+                hintText: 'Entre com o seu login',               
+                label: Text('Login')
+              ),
+              controller: _loginController,
             ),
-            const SizedBox(height: 30.0,),
-            const Text('Login'),
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: TextField(
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'Entre com o seu login',
-                ),
-                controller: _loginController,
-                ),
+          ),
+          Text(_msgCampoLogin, style: const TextStyle(color: Colors.red)),
+          const SizedBox(
+            height: 15.0,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: TextField(
+              controller: _senhaController,
+              obscureText: true,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Digite a sua senha',
+                label: Text('senha')
+              ),
             ),
-            const SizedBox(height: 15.0,),
-            const Text('Senha'),
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: TextField(
-                controller: _senhaController,
-                obscureText: true,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'Digite a sua senha',
-                ),),
-            ),
-            const SizedBox(height: 15.0,),
-            ElevatedButton(
-                onPressed: ()async{                
-                  if(await _existeUsuarioCadastrado()){
-                    _loginController.text = "";
-                    _senhaController.text = "";
-                    Navigator.pushNamed(context, "menu");
-                  }                                 
-                }, 
-                child: const Text('Fazer login')),
-            TextButton(
-              onPressed: (){
+          ),
+          Text(
+            _msgCampoSenha,
+            style: const TextStyle(color: Colors.red),
+          ),
+          Text(
+            _msgExisteUsuario,
+            style: const TextStyle(color: Colors.red),
+          ),
+          const SizedBox(
+            height: 15.0,
+          ),
+          ElevatedButton(
+              onPressed: () async {
+                if (await _existeUsuarioCadastrado()) {
+                  _limparCampos();
+                  Navigator.pushNamed(context, "menu");
+                } else if (_msgCampoLogin.isEmpty && _msgCampoSenha.isEmpty) {
+                  _msgExisteUsuario = "usuário não cadastrado!";
+                }
+              },
+              child: const Text('Fazer login')),
+          TextButton(
+              onPressed: () {
+                _limparCampos();
                 Navigator.pushNamed(context, "cadastro");
-              }, 
-              child: const Text('Não possui conta? Cadastre-se!', 
-                              style: TextStyle(decoration: TextDecoration.underline),))
-          ],
-        )),
+              },
+              child: const Text(
+                'Não possui conta? Cadastre-se!',
+                style: TextStyle(decoration: TextDecoration.underline),
+              ))
+        ],
+      )),
     );
   }
 }
